db83f5d0c0794d6963a96b4f756b70e51e417859
MANYIKAM <manyikamprogramer@gmail.com>
2022-10-17 15:59:28 +0200

    added js code to the portfolio

---
+++ index.js
@@ -0,0 +1,19 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+// Toggle menu on Humberger Button click
+const menus = document.querySelectorAll('.toggle');
+const menusArray = Array.from(menus);
+function toggleMenu() {
+  menusArray.forEach((element) => {
+    element.classList.toggle('menu');
+  });
+  document.querySelector('.top-navigation .name span').classList.toggle('menu');
+  document.querySelector('.top-navigation .name .fa-bars').classList.toggle('menu');
+  document.querySelector('.top-navigation .name .fa-times').classList.toggle('menu');
+}
+// Hide menu on Click
+menusArray.forEach((element) => {
+  element.addEventListener('click', () => {
+    toggleMenu();
+  });
+});
