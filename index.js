9af82053d92f07262c9531e95b030ac46a6050ad
pius ssekweyama <ssekweyamapius@gmail.com>
2022-10-18 17:53:49 +0300

    fix eslint stylelint

--- index.js
+++ index.js
@@ -1,62 +1,9 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
-// Toggle menu on Humberger Button click
-const menus = document.querySelectorAll('.toggle');
+const menus = document.querySelectorAll('.toggle');// Toggle menu on Humberger Button click
 const menusArray = Array.from(menus);
-function toggleMenu() {
-  menusArray.forEach((element) => {
-    element.classList.toggle('menu');
-  });
-  document.querySelector('.top-navigation .name span').classList.toggle('menu');
-  document.querySelector('.top-navigation .name .fa-bars').classList.toggle('menu');
-  document.querySelector('.top-navigation .name .fa-times').classList.toggle('menu');
-  document.querySelector('.top-navigation').classList.toggle('fullHeight');
-}
-// Hide menu on Click
-menusArray.forEach((element) => {
-  element.addEventListener('click', () => {
-    toggleMenu();
-  });
-});
-// launch projects modal
-let modalContent = {
-  header: '',
-  technologies: [],
-  description: '',
 
-};
-
-document.querySelector('.modal-content i').addEventListener('click', (event) => {
-  event.preventDefault();
-  modalContent = {
-    header: '',
-    technologies: [],
-    description: '',
-
-  };
-  document.querySelector('.modal').classList.toggle('hide');
-});
-
-function launchProject(id) {
-  if (id >= 0) {
-    modalContent.header = projects[id].name;
-    modalContent.technologies = projects[id].technologies;
-    modalContent.description = projects[id].description;
-
-    document.querySelector('.modal').classList.toggle('hide');
-    document.querySelector('.modal-content h2').innerText = modalContent.header;
-    let ul_content = '';
-    modalContent.technologies.forEach((item) => {
-      ul_content += `<li><span>${item}</span></li>`;
-    });
-    const ul = document.querySelector('.modal-content ul');
-    ul.innerHTML = ul_content;
-    document.querySelector('.modal-content-body p').innerText = modalContent.description;
-  }
-}
-
-// projects
-let projects = [
+const projects = [// projects
   {
     name: 'Multi-Post Stories',
     description: `A daily selection of privately personalized reads; no
@@ -127,20 +74,72 @@ let projects = [
     link_source: '#',
   },
 ];
-// load projects
-const container = document.querySelector('#content');
 
-const ul_tiles = document.createElement('ul');
-ul_tiles.classList.add('grid-container');
+function toggleMenu() {
+  menusArray.forEach((element) => {
+    element.classList.toggle('menu');
+  });
+  document.querySelector('.top-navigation .name span').classList.toggle('menu');
+  document.querySelector('.top-navigation .name .fa-bars').classList.toggle('menu');
+  document.querySelector('.top-navigation .name .fa-times').classList.toggle('menu');
+  document.querySelector('.top-navigation').classList.toggle('fullHeight');
+}
+
+menusArray.forEach((element) => { // Hide menu on Click
+  element.addEventListener('click', () => {
+    toggleMenu();
+  });
+});
+
+let modalContent = { // launch projects modal
+  header: '',
+  technologies: [],
+  description: '',
+
+};
+
+document.querySelector('.modal-content i').addEventListener('click', (event) => {
+  event.preventDefault();
+  modalContent = {
+    header: '',
+    technologies: [],
+    description: '',
+
+  };
+  document.querySelector('.modal').classList.toggle('hide');
+});
+
+function launchProject(id) {
+  if (id >= 0) {
+    modalContent.header = projects[id].name;
+    modalContent.technologies = projects[id].technologies;
+    modalContent.description = projects[id].description;
+
+    document.querySelector('.modal').classList.toggle('hide');
+    document.querySelector('.modal-content h2').innerText = modalContent.header;
+    let ulContent = '';
+    modalContent.technologies.forEach((item) => {
+      ulContent += `<li><span>${item}</span></li>`;
+    });
+    const ul = document.querySelector('.modal-content ul');
+    ul.innerHTML = ulContent;
+    document.querySelector('.modal-content-body p').innerText = modalContent.description;
+  }
+}
+
+const container = document.querySelector('#content');// load projects
+
+const ulTiles = document.createElement('ul');
+ulTiles.classList.add('grid-container');
 
 projects.forEach((project, index, projectArray) => {
-  if (index == 0) {
+  if (index === 0) {
     const img = document.createElement('img');
     img.src = project.image;
     img.alt = 'Image placeholder';
-    const card_left = document.createElement('div');
-    card_left.classList.add('card-left');
-    card_left.appendChild(img);
+    const cardLeft = document.createElement('div');
+    cardLeft.classList.add('card-left');
+    cardLeft.appendChild(img);
 
     const h3 = document.createElement('h3');
     h3.innerText = 'Multi-Post Stories';
@@ -163,20 +162,20 @@ projects.forEach((project, index, projectArray) => {
     input.value = 'See Project';
     input.addEventListener('click', () => launchProject(0));
 
-    const card_right = document.createElement('div');
-    card_right.classList.add('card-right');
-    card_right.appendChild(h3);
-    card_right.appendChild(p);
-    card_right.appendChild(dv);
-    card_right.appendChild(input);
+    const cardRight = document.createElement('div');
+    cardRight.classList.add('card-right');
+    cardRight.appendChild(h3);
+    cardRight.appendChild(p);
+    cardRight.appendChild(dv);
+    cardRight.appendChild(input);
 
-    const card_works = document.createElement('div');
-    card_works.classList.add('card-works');
-    card_works.appendChild(card_left);
-    card_works.appendChild(card_right);
+    const cardWorks = document.createElement('div');
+    cardWorks.classList.add('card-works');
+    cardWorks.appendChild(cardLeft);
+    cardWorks.appendChild(cardRight);
 
-    container.appendChild(card_works);
-  } else if (index == 1) {
+    container.appendChild(cardWorks);
+  } else if (index === 1) {
     const input = document.createElement('input');
     input.type = 'submit';
     input.value = 'See Project';
@@ -190,7 +189,7 @@ projects.forEach((project, index, projectArray) => {
     const li = document.createElement('li');
     li.appendChild(dv);
 
-    ul_tiles.appendChild(li);
+    ulTiles.appendChild(li);
   } else {
     const span1 = document.createElement('span');
     span1.classList.add('title-post');
@@ -220,11 +219,11 @@ projects.forEach((project, index, projectArray) => {
     const li = document.createElement('li');
     li.appendChild(dv);
 
-    ul_tiles.appendChild(li);
+    ulTiles.appendChild(li);
   }
 });
 
-const card_works_tiles = document.createElement('div');
-card_works_tiles.classList.add('card-works');
-card_works_tiles.appendChild(ul_tiles);
-container.appendChild(card_works_tiles);
+const cardWorksTiles = document.createElement('div');
+cardWorksTiles.classList.add('card-works');
+cardWorksTiles.appendChild(ulTiles);
+container.appendChild(cardWorksTiles);
